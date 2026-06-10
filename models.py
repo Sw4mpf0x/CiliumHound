ba79aa2104485fa6bf5e2c7c6fad132a948fa117
Andrew Luke <andrew.eric.luke@gmail.com>
2026-06-10 09:48:51 -0700

    Modified hash generation to include the rules property if present

--- models.py
+++ models.py
@@ -29,26 +29,43 @@ class Ports:
         print(f"Ports: {self.ports}")
         print(f"DNS Rules: {self.dns_rules}")
 
-def append_key_hash_id(key: str, namespace: str, direction: str) -> str:
-    key_hash = hashlib.sha256(f"{namespace}:{direction}".encode("utf-8")).hexdigest()[-8:]
+def append_key_hash_id(key: str, namespace: str, direction: str, rules: str = None) -> str:
+    if rules:
+        hash_input = f"{namespace}:{direction}:{rules}"
+    else:
+        hash_input = f"{namespace}:{direction}"
+
+    key_hash = hashlib.sha256(hash_input.encode("utf-8")).hexdigest()[-8:]
     return f"{key}-{key_hash}"
 
 class Rule:
     direction: str
     namespace: str
     key: str = None
+    rule_type: str
     edge_type: EdgeType = None
     endpoint_selector: str = None
     to_ports: Ports = None
     tgt_namespace: str = None
     properties: Dict[str, Any]
 
-    def __init__(self, direction: str, namespace: str, key: str, edge_type: EdgeType = None):
+    def __init__(self, direction: str, namespace: str, key: str, rule_type, properties: dict = {}, edge_type: EdgeType = None):
         self.direction = direction
         self.namespace = namespace
-        self.key = append_key_hash_id(key, namespace, direction)
+        self.rule_type = rule_type
+        self.properties = properties
         self.edge_type = edge_type
-        self.properties = {}
+        self.key = key
+        self.generate_key_identifier()
+
+    def generate_key_identifier(self):
+        # If this is just a namespace rule, we don't want include the ID suffix
+        if self.rule_type in "namespace":
+            return
+        elif self.properties.get("rules"):
+            self.key = append_key_hash_id(self.key, self.namespace, self.direction, rules=self.properties.get("rules"))
+        else:
+            self.key = append_key_hash_id(self.key, self.namespace, self.direction)
 
     def print(self):
         print("-" * 80)
@@ -59,6 +76,7 @@ class Rule:
         print(f"Endpoint Selector: {self.endpoint_selector}")
         if self.to_ports:
             self.to_ports.print()
+        print(f"Rule Type: {self.rule_type}")
         print(f"To Namespace: {self.tgt_namespace}")
         print(f"Properties: {self.properties}")
         print("-" * 80)
